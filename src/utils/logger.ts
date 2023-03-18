d59ee9cc754577e7c4c7352192c7fc85a0e30e33
tsuchipls <jdjneke@gmail.com>
2023-03-18 12:00:21 -0300

    🎉 First

---
+++ src/utils/logger.ts
@@ -0,0 +1,9 @@
+import { Logger } from "tslog";
+export default new Logger({
+    exposeStack: false,
+    displayFunctionName: false,
+    displayInstanceName: false,
+
+    dateTimePattern: 'year-day-month hour:minute',
+    displayFilePath: 'displayAll'
+})
