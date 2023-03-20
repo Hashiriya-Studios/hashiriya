3ff02214d8b97cc723013e6390da30e6eff358a5
asolden22 <imfigliuolo@gmail.com>
2023-03-19 21:11:23 -0300

    🐛 fix again

--- src/server/routes/errorEndpoint.ts
+++ src/server/routes/errorEndpoint.ts
@@ -3,5 +3,5 @@ import express from 'express';
 import { join } from 'path';
 
 export default express.Router().get('/', (req, res) => {
-    res.redirect(404, join(process.cwd(), 'public', 'resources', 'main.html'));
+    res.status(404).sendFile(join(process.cwd(), 'public', 'resources', 'main.html'));
 });
