68fc297975122a24780c08e6c23e766be2c2c574
asolden22 <imfigliuolo@gmail.com>
2023-03-20 11:32:07 -0300

    🐛 Fixing things

--- src/server/app.ts
+++ src/server/app.ts
@@ -8,7 +8,7 @@ import mainEndpoint from './routes/mainEndpoint';
 import errorEndpoint from './routes/errorEndpoint';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 const rateLimit = ratelimit({
     max: 20,
     windowMs: 60000,
