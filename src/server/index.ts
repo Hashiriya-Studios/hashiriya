71c799dae88103e53d8cf5c3c95e03c5bbda309a
tsuchipls <jdjneke@gmail.com>
2023-03-23 23:07:20 -0300

    NEED: /auth and register

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,26 +1,30 @@
 import express from 'express';
 import ratelimit from 'express-rate-limit';
 import logger from '../utils/logger';
+import morgan from 'morgan'
 
 import { join } from 'path';
 
 import mainEndpoint from './routes/mainEndpoint';
 import errorEndpoint from './routes/errorEndpoint';
+import loginEndpoint from './routes/loginEndpoint';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const rateLimit = ratelimit({
-    max: 20,
+    max: 100,
     windowMs: 60000,
     message: 'Too much requests.'
 });
 // Middleware
 app.set('view engine', 'ejs')
+app.use(morgan('dev'))
 app.use(rateLimit);
 app.use(express.static(join(process.cwd(), 'views')));
 
 // Endpoints
 app.use('/', mainEndpoint);
+app.use('/login', loginEndpoint);
 app.use('*', errorEndpoint); // Error Endpoint
 
 app.listen(port, () => {
--- src/server/routes/loginEndpoint.ts
+++ src/server/routes/loginEndpoint.ts
@@ -3,5 +3,5 @@ import express from 'express';
 import { join } from 'path';
 
 export default express.Router().get('/', (req, res) => {
-    res.render('resources/main.ejs');
+    res.render('resources/user/login.ejs');
 });
